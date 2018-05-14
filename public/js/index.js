ce61b382e787c957ab04c82c6094ebed747a0e7c
Jacob <jerling1989@gmail.com>
2018-05-14 17:31:33 -0400

    message form and jQuery message DOM

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 // CREATE SOCKET CONNECTION
 var socket = io();
+
 // SERVER CONENECTION EVENT LISTENER
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
+
 // SERVER DISCONNECTED EVENT LISTENER
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
@@ -12,12 +14,21 @@ socket.on('disconnect', function () {
 // NEW MESSAGE EVENT LISTENER
 socket.on('newMessage', function (message) {
 	console.log('newMessage', message);
+
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
 });
 
+// EVENT LISTENER FOR MESSAGE FORM SUBMISSION
+jQuery('#message-form').on('submit', function (e) {
+	e.preventDefault();
 
-socket.emit('createMessage', {
-	from: 'Frank',
-	text: 'Hi'
-}, function (data) {
-	console.log('Got it!', data);
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
 });
